aa563aef5113095f4eb972fb5068af5cd157e74c
oezdemir <ozdemir@cip.ifi.lmu.de>
2025-06-15 21:30:03 +0200

    Remove logging of the data

--- frontend/modules/MainController.js
+++ frontend/modules/MainController.js
@@ -575,8 +575,8 @@ class MainController {
           this.updateEmotions();
           this.updateVisualizations();
           this.updateSentenceList();
-          console.log('Restored saved data from localStorage');
-          console.log("Dynamic analysis JSON result:", this.data);          
+          //console.log('Restored saved data from localStorage');
+          //console.log("Dynamic analysis JSON result:", this.data);          
 
           return true;
         }
